e84e5f3ca2e485637fb4ac87b6e530a2a050f295
collinwilkins <wilkins507@gmail.com>
2025-10-05 13:28:48 -0400

    update index pages to resolve assets

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/'
+  base: '/my-portfolio/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true
+  }
 })
